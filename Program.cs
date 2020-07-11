69c9817c9fce24ab8cc8905a30307bf9ded75faf
cuong012qn <cuong012qn121@gmail.com>
2020-07-11 13:02:01 +0700

    update

--- Program.cs
+++ Program.cs
@@ -6,9 +6,16 @@ namespace MidTerm
     {
         static void Main(string[] args)
         {
-            var honso1 = new HonSo(20, 90, 50);
-            var honso2 = new HonSo(12, 23, 30);
-            Console.WriteLine(honso1.Tru(honso2).ConvertToString());
+            HonSo honso1 = new HonSo(20, 90, 50);
+            HonSo honso2 = new HonSo(12, 23, 30);
+            Console.WriteLine(honso1.ConvertToString());
+            Console.WriteLine(honso2.ConvertToString());
+            Console.WriteLine("{0} + {1} = {2}", honso1.ConvertToString(), honso2.ConvertToString(),
+                honso1.Cong(honso2).ConvertToString());
+            Console.WriteLine("{0} - {1} = {2}", honso1.ConvertToString(), honso2.ConvertToString(),
+                honso1.Tru(honso2).ConvertToString());
+            Console.WriteLine("Gia tri so thuc " + honso1.GiaTri());
+            Console.WriteLine("Nghich dao " + honso1.NghichDao());
             Console.ReadKey();
         }
 
